ac0714c9694f0ddd114b5e6e78f893a8ea44f84b
shuvo <hrshuva9@gmail.com>
2021-12-14 14:21:17 +0600

    solved top 7 problem

---
+++ src/ProblemSolvingTask/task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            str= Console.ReadLine();
+            char[] stra = str.ToCharArray();
+            str = Console.ReadLine();
+            char[] strb = str.ToCharArray();
+            
+            int l = stra.Length;
+ 
+            for (int i = 0; i < l; i++)
+            {
+                char a = Char.ToLower(stra[i]);
+                char b = Char.ToLower(strb[i]);
+ 
+                if (a < b)
+                {
+                    Console.WriteLine(-1);
+                    return;
+                }
+                else if (a > b)
+                {
+                    Console.WriteLine(1);
+                    return;
+                }
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
---
+++ src/ProblemSolvingTask/task_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            string str;
+            str= Console.ReadLine();
+            char[] s = str.ToCharArray();
+ 
+            int cnt = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    cnt++;
+                }
+            }
+ 
+            Console.WriteLine(cnt);
+        }
+    }
+}
