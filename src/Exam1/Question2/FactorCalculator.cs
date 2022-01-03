0edd41c7d73bae7668639e0abdb490957638aa74
shuvo <hrshuva9@gmail.com>
2022-01-03 21:58:21 +0600

    completed task 2

--- src/Exam1/Question2/FactorCalculator.cs
+++ src/Exam1/Question2/FactorCalculator.cs
@@ -10,7 +10,38 @@ namespace Question2
     {
         public static int[] FindFactors(int number)
         {
-            throw new NotImplementedException();
+            bool[] isPrime = new bool[35000]; // false = prime
+            isPrime[1] = true;
+            for (int i = 2; i < 35000; i++)
+            {
+                if (isPrime[i] == false)
+                {
+                    for (int j = i * i; j < 35000; j += i)
+                    {
+                        isPrime[j] = true;
+                    }
+                }
+            }
+            
+            int[] factors = new int[350000];
+            int k = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (isPrime[i] == false && number % i == 0)
+                {
+                    factors[k++] = i;
+                }
+            }
+
+            int[] Factors = new int[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                Factors[i] = factors[i];
+            }
+
+            return Factors;
         }
     }
 }
--- src/Exam1/Question2/Program.cs
+++ src/Exam1/Question2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Question2;
+
 Console.WriteLine("Hello, World!");
+
+
+FactorCalculator f = new FactorCalculator();
+
+int[] arr = FactorCalculator.FindFactors(144);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
