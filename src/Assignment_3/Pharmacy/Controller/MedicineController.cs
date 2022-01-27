cffb0077b186915d5b105f41cb59927f931140f3
shuvo <hrshuva9@gmail.com>
2022-01-28 00:45:10 +0600

    added update, search

--- src/Assignment_3/Pharmacy/Controller/MedicineController.cs
+++ src/Assignment_3/Pharmacy/Controller/MedicineController.cs
@@ -10,12 +10,78 @@ public class MedicineController
     public void Create()
     {
         Medicine medicine = new Medicine();
-        
+
+        Console.WriteLine("Enter medicine name: ");
         medicine.Name = Console.ReadLine();
+        Console.WriteLine("Enter medicine quantity: ");
         medicine.Quantity = Convert.ToInt32(Console.ReadLine());
 
         _dbContext.Add(medicine);
         _dbContext.SaveChanges();
         Console.WriteLine("Medecine \"" + medicine.Name + "\" with Quantity \"" + medicine.Quantity + "\" added Successfully!");
     }
+    
+    public void Search()
+    {
+        Console.Write("Enter medicine name to search: ");
+        string name = Console.ReadLine();
+
+        var medicines = from m in _dbContext.Medicines select m;
+
+        if (!String.IsNullOrEmpty(name))
+        {
+            medicines = medicines.Where(m => m.Name != null && m.Name.Contains(name));
+
+            if (!medicines.Any())
+            {
+                Console.WriteLine("Not Found any medicine with " + name + " keyword, Add new");
+                Create();
+                return;
+            }
+        }
+        else
+        {
+            Search();
+            return;
+        }
+
+        Console.WriteLine("  Medicine Name       Quantity");
+        foreach (var m in medicines)
+        {
+            Console.Write("      " + m.Name);
+            if (m.Name != null)
+                for (var i = 0; i < 20 - m.Name.Length; i++)
+                {
+                    Console.Write(" ");
+                }
+
+            Console.WriteLine(m.Quantity);
+        }
+
+    }
+
+    public void Update()
+    {
+        Console.Write("Enter medicine name: ");
+        string? name = Console.ReadLine();
+
+        Medicine medicine = _dbContext.Medicines.FirstOrDefault(m => m.Name.Contains(name));
+        Console.WriteLine(medicine.Name);
+        
+        Medicine newMedicine = new Medicine();
+
+        Console.WriteLine("Enter medicine name: ");
+        newMedicine.Name = Console.ReadLine();
+        Console.WriteLine("Enter medicine quantity: ");
+        newMedicine.Quantity = Convert.ToInt32(Console.ReadLine());
+
+        medicine.Name = newMedicine.Name;
+        medicine.Quantity = newMedicine.Quantity;
+
+        _dbContext.SaveChanges();
+
+        Console.WriteLine("Medicine updated successfully!");
+    }
+    
+    
 }
--- src/Assignment_3/Pharmacy/Program.cs
+++ src/Assignment_3/Pharmacy/Program.cs
@@ -10,20 +10,34 @@ MedicineController medicine = new MedicineController();
 while (true)
 {
     Console.Write("\nEnter option: ");
-    char input = Char.ToLower(Console.ReadLine()[0]);
-    
+    // char input = Char.ToLower(Console.ReadLine()[0]);
+
+    char input;
+    try
+    {
+        input = Char.ToLower(Console.ReadLine()[0]);
+    }
+    catch (Exception e)
+    {
+        continue;
+    }
 
     switch (input)
     {
         case 'a':
             Console.WriteLine("a. Search medicine");
+            medicine.Search();
+            Console.WriteLine();
             break;
         case 'b':
             Console.WriteLine("b. Add Medicine");
             medicine.Create();
+            Console.WriteLine();
             break;
         case 'c':
             Console.WriteLine("c. Update Medicine");
+            medicine.Update();
+            Console.WriteLine();
             break;
         case 'd':
             Console.WriteLine("d. Remove Medicine");
@@ -34,6 +48,7 @@ while (true)
         default:
             dashboard.Display();
             Console.WriteLine("[warning] Enter option among [a-e]");
+            Console.WriteLine();
             break;
     }
 
