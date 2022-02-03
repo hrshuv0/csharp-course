c04cde55906b96064aae960fb3295666d73e0ffa
shuvo <hrshuva9@gmail.com>
2022-02-03 21:08:18 +0600

    cloned exam3

---
+++ src/Exam3/Question1/DirectoryUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    internal class DirectoryUtility
+    {
+        public static string[] GetFileNames(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Exam3/Question3/FileWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    internal class FileWriter
+    {
+        public void WriteToFile(List<string> lines, string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
